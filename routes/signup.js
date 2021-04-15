99ddd56aad97a2f47ee3fe2a5e274f9a30db197d
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-15 15:33:48 +0530

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -1,24 +1,37 @@
 const { json } = require('body-parser');
 const express = require('express');
 const Signupdetails = require('../Model/Signupdetails');
-
 const router = express.Router();
 
-const signup = require('../Model/Signupdetails');
-
 router.post('/' ,(req,res) => {
-    const signupdetails = new Signupdetails(req.body)
-    
     email = req.body.email
-    user = email.substring(0,email.lastIndexOf("@")) 
+    const signupdetails = new Signupdetails({
+        email : req.body.email ,
+        userName : email.substring(0,email.lastIndexOf("@")),
+        PersonalInformation : {
+            firstName : req.body.PersonalInformation.firstName ,
+            lastName : req.body.PersonalInformation.lastName ,
+            phoneNumber : req.body.PersonalInformation.phoneNumber,
+            DOB : req.body.PersonalInformation.DOB 
+        } ,
 
-    signupdetails.save() . then ( data => {
-        res.status(200).json(user);
-    })
-
-    .catch (err => {
-        res.json({ message : err });
+        Address : {
+            street : req.body.Address.street ,
+            city : req.body.Address.city ,
+            pincode : req.body.Address.pincode,
+            state : req.body.Address.state,
+            country : req.body.Address.country
+        }
     });
+    userName = signupdetails.userName
+    password =signupdetails.password
+    signupdetails.save() 
+        .then (doc =>  {
+            res.status(200).json({USERNAME : userName , PASSWORD : password });
+        })
+        .catch (err => {
+            res.json({ message : err });
+        });
 });
 
 
