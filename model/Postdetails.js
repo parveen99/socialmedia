5209fa071e08cbf04544bd51151d3547f02e3794
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-17 02:11:09 +0530

    Create Postdetails.js

---
+++ model/Postdetails.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const postschema = mongoose.Schema({ 
+    userName :{
+        type : String,
+        required : true
+    },
+    status : {
+        type : String,
+        max:150
+    },
+    likes : {
+        type : Array ,
+        default : []
+    }
+},
+{timestamps :{ createdAt: 'created_at',updatedAt: 'updated_at'}}
+);
+
+module.exports = mongoose.model('Post' , postschema);
