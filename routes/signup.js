308b63a314858f44be269a204980cc9391439b23
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-16 23:55:24 +0530

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -3,36 +3,37 @@ const express = require('express');
 const Signupdetails = require('../Model/Signupdetails');
 const router = express.Router();
 
-router.post('/' ,(req,res) => {
+
+//USER SIGNUP
+router.post('/' , async (req,res) => {
     email = req.body.email
     const signupdetails = new Signupdetails({
         email : req.body.email ,
         userName : email.substring(0,email.lastIndexOf("@")),
-        PersonalInformation : {
-            firstName : req.body.PersonalInformation.firstName ,
-            lastName : req.body.PersonalInformation.lastName ,
-            phoneNumber : req.body.PersonalInformation.phoneNumber,
-            DOB : req.body.PersonalInformation.DOB 
+        personalInformation : {
+            firstName : req.body.personalInformation.firstName ,
+            lastName : req.body.personalInformation.lastName ,
+            phoneNumber : req.body.personalInformation.phoneNumber,
+            DOB : req.body.personalInformation.DOB 
         } ,
 
-        Address : {
-            street : req.body.Address.street ,
-            city : req.body.Address.city ,
-            pincode : req.body.Address.pincode,
-            state : req.body.Address.state,
-            country : req.body.Address.country
+        address : {
+            street : req.body.address.street ,
+            city : req.body.address.city ,
+            pincode : req.body.address.pincode,
+            state : req.body.address.state,
+            country : req.body.address.country
         }
     });
     userName = signupdetails.userName
     password =signupdetails.password
-    signupdetails.save() 
-        .then (doc =>  {
-            res.status(200).json({USERNAME : userName , PASSWORD : password });
-        })
-        .catch (err => {
-            res.json({ message : err });
-        });
-});
 
+    try{
+    await signupdetails.save() 
+    res.status(201).json({USERNAME : userName , PASSWORD : password });
+    } catch (err){
+        res.status(500).json({message : err});
+    }
+});
 
 module.exports = router ;
