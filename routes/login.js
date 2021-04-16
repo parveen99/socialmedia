e41bc1346da1c182f09e518f998e75a520ef081d
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-16 23:55:50 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -6,47 +6,83 @@ const router = express.Router();
 //LOGIN USING USERNAME / PASSWORD
 router.get('/' ,async (req,res) => {
     try{
-        const login = await Signupdetails.find(req.body);
+        if(req.body.email){
+        var login = await Signupdetails.findOne({email : req.body.email});
+        !login && res.status(400).json({message : "User not found! Invalid emailID"});
+        }
 
-        if((req.body.userName === login[0].userName || req.body.email === login[0].email) && req.body.password ===login[0].password ){
-            res.json({message : "Login Successfull"});
+        else{
+        var login = await Signupdetails.findOne({userName : req.body.userName});
+        !login && res.status(400).json({message : "User not found! Invalid user"});
+        }
+
+        if(req.body.userName === login.userName || req.body.email === login.email){
+            if(req.body.password ===login.password){
+                res.json({message : "Login Successfull"});
+            }
+            else{
+                res.json({message : "Invalid Password"});
+            }
         }
     }
     catch (err) {
         res.status(500).json({message : err});
     }
-    
 });
 
 //UPDATING PASSWORD
-router.patch('/update' ,async (req,res) => {
-    try{
-        const login = await Signupdetails.updateOne(
-            {userName : req.body.userName} ,
-            {$set:{password : req.body.password}}
-        );
-        res.json({message : "Password updated successfully"});
+router.patch('/:username' ,async (req,res) => {
+    if(req.body.userName === req.params.username){
+        try{
+            await Signupdetails.updateOne(
+                {userName : req.body.userName} ,
+                {$set:{password : req.body.password}}
+            );
+            res.json({message : "Password updated successfully"});
+        }
+        catch (err){
+            res.status(500).json({message : err});
+        }
     }
-    catch (err){
-        res.status(500).json({message : err});
+    else{
+        return res.json({message:"You can update only your password"});
     }
-
 });
-
-
 
 //DELETING A USER
 router.delete('/:username' , async (req, res) => {
-    try{
-        await Signupdetails.remove({userName : req.params.username });
-        res.json({message : "User removed successfully"});
+    if(req.body.userName === req.params.username){
+        try{
+            await Signupdetails.remove({userName : req.params.username });
+            res.json({message : "User removed successfully"});
+        }
+        catch (err){
+            res.status(500).json({message : err});
+        }
     }
-    catch (err){
-        res.status(500).json({message : err});
+    else{
+        return res.json({message : "You can only delete your account"});
     }
-
 });
 
 
+//UPDATING ANY INFORMATION
+
+router.patch("/updateany/:username" , async (req,res) => {
+    if(req.body.userName === req.params.username){
+        try{
+            await Signupdetails.findOneAndUpdate(
+                {userName:req.body.userName},
+                {$set:req.body});
+            res.status(200).json({message : "User Details Updated Successfully"});
+        }
+        catch (err) {
+            res.status(500).json({message : err});
+        }
+    }
+    else{
+        return res.json({message : "You can update only your account"});
+    }
+});
 
 module.exports = router ;
