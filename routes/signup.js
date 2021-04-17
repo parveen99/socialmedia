19365caf4032302099337927bbbe1bd9cb151320
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-17 23:23:47 +0530

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -3,10 +3,15 @@ const express = require('express');
 const Signupdetails = require('../Model/Signupdetails');
 const router = express.Router();
 
-
 //USER SIGNUP
 router.post('/' , async (req,res) => {
     email = req.body.email
+    DOB = req.body.personalInformation.DOB ;
+    function calculateAge(DOB){
+        var ageDifMs = Date.now() - DOB.getTime();
+        var ageDate = new Date(ageDifMs);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
     const signupdetails = new Signupdetails({
         email : req.body.email ,
         userName : email.substring(0,email.lastIndexOf("@")),
@@ -14,9 +19,9 @@ router.post('/' , async (req,res) => {
             firstName : req.body.personalInformation.firstName ,
             lastName : req.body.personalInformation.lastName ,
             phoneNumber : req.body.personalInformation.phoneNumber,
-            DOB : req.body.personalInformation.DOB 
+            DOB : req.body.personalInformation.DOB,
+            age : calculateAge(new Date(DOB))
         } ,
-
         address : {
             street : req.body.address.street ,
             city : req.body.address.city ,
@@ -31,7 +36,7 @@ router.post('/' , async (req,res) => {
     try{
     await signupdetails.save() 
     res.status(201).json({USERNAME : userName , PASSWORD : password });
-    } catch (err){
+    } catch (err) {
         res.status(500).json({message : err});
     }
 });
