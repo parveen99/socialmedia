db51d5de8f3bfe31fbe04207d951c8ad3174834c
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-29 02:30:16 +0530

    Update user.test.js

--- Tests/user.test.js
+++ Tests/user.test.js
@@ -1,11 +1,5 @@
 
 
-
-
-
-
-
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let app = require ('../app');
@@ -18,11 +12,11 @@ chai.use(chaiHttp);
 
 
 describe('API Login', ()=> {
-    describe ("GET /", ()=> {
+    describe ("GET User Login/", ()=> {
         it("Login successfull with userName and Password", (done) =>{
             chai.request(app)
             .get("/user")
-            .send({userName : "Sameena", password : "social"})
+            .send({userName : "waheedha", password : "social"})
             .end((err,response)=>{
               response.should.have.status(200);
               response.body.should.be.a('object');
@@ -32,11 +26,11 @@ describe('API Login', ()=> {
         });
     });
 
-  describe ("GET /", ()=> {
+  describe ("GET User Login/", ()=> {
     it("Login successfull with Email and Password", (done) =>{
       chai.request(app)
       .get("/user")
-      .send({email : "Sameena@gmail.com", password : "social"})
+      .send({email : "waheedha@gmail.com", password : "social"})
       .end((err,response)=>{
         response.should.have.status(200);
         response.body.should.be.a('object');
@@ -51,7 +45,7 @@ describe('API Login', ()=> {
     it("Wrong API - Bad Request", (done) =>{
       chai.request(app)
       .get("/users")
-      .send({userName : "Sameena", password : "social"})
+      .send({userName : "Sameena", password : "socialgram"})
       .end((err,response)=>{
         response.should.have.status(404);
         done();
@@ -60,12 +54,13 @@ describe('API Login', ()=> {
   });
 
 
-  describe ("GET /", ()=> {
+  describe ("GET User Login/", ()=> {
     it("Username correct password wrong", (done) =>{
       chai.request(app)
       .get("/user")
       .send({userName : "Sameena", password : "socialgram"})
       .end((err,response)=>{
+        response.should.have.status(401);
         response.body.should.be.a('object');
         response.body.should.have.property('message').eq('UserName / Password Incorrect');
         done();
@@ -73,12 +68,13 @@ describe('API Login', ()=> {
     });
   });
 
-  describe ("GET /", ()=> {
+  describe ("GET User Login/", ()=> {
     it("Email correct password wrong", (done) =>{
       chai.request(app)
       .get("/user")
       .send({email : "Sameena@gmail.com", password : "socialgram"})
       .end((err,response)=>{
+        response.should.have.status(401);
         response.body.should.be.a('object');
         response.body.should.have.property('message').eq("Email / Password Incorrect");
         done();
@@ -86,7 +82,7 @@ describe('API Login', ()=> {
     });
   });
 
-  describe ("GET /", ()=> {
+  describe ("GET User Login/", ()=> {
     it("Email invalid", (done) =>{
       chai.request(app)
       .get("/user")
@@ -100,7 +96,7 @@ describe('API Login', ()=> {
     });
   });
 
-  describe ("GET /", ()=> {
+  describe ("GET User Login/", ()=> {
     it("Username invalid", (done) =>{
       chai.request(app)
       .get("/user")
@@ -118,191 +114,195 @@ describe('API Login', ()=> {
 
 
 
-// describe('API Update Password', ()=> {
-//   describe ("PATCH /", ()=> {
-//       it("Update Password Success", (done) =>{
-//           chai.request(app)
-//           .patch("/user/:username")
-//           .send({userName : "Sameena", password : "tissue"})
-//           .end((err,response)=>{
-//             response.should.have.status(200);
-//             response.body.should.be.a('object');
-//             response.body.should.have.property('message').eq('Password updated successfully');
-//             done();
-//           });
-//       });
-//   });
+describe('API Update Password', ()=> {
+  describe ("PATCH /", ()=> {
+      it("Update Password Success", (done) =>{
+          chai.request(app)
+          .patch("/user/" + 'Sameena')
+          .send({userName : "Sameena", password : "tissue"})
+          .end((err,response)=>{
+            response.should.have.status(200);
+            response.body.should.be.a('object');
+            response.body.should.have.property('message').eq('Password updated successfully');
+            done();
+          });
+      });
+  });
 
-//   describe ("PATCH /", ()=> {
-//     it("Update Password Fail - No userName", (done) =>{
-//         chai.request(app)
-//         .patch("/user/:username")
-//         .send({password : "social"})
-//         .end((err,response)=>{
-//           response.body.should.be.a('object');
-//           response.body.should.have.property('message').eq('userName is required');
-//           done();
-//         });
-//     });
-// });
+  describe ("PATCH Update Password/", ()=> {
+    it("Update Password Fail - No userName", (done) =>{
+        chai.request(app)
+        .patch("/user/" + 'Sameena')
+        .send({password : "social"})
+        .end((err,response)=>{
+          response.should.have.status(400);
+          response.body.should.be.a('object');
+          response.body.should.have.property('message').eq('userName is required');
+          done();
+        });
+    });
+});
 
-// describe ("PATCH /", ()=> {
-//   it("Update Password Fail - No password", (done) =>{
-//       chai.request(app)
-//       .patch("/user/:username")
-//       .send({userName : "Sameena"})
-//       .end((err,response)=>{
-//         response.body.should.be.a('object');
-//         response.body.should.have.property('message').eq('Enter password to update');
-//         done();
-//       });
-//   });
-// });
-
-
-// describe ("PATCH /", ()=> {
-//   it("Update Password Fail - Bad request", (done) =>{
-//       chai.request(app)
-//       .patch("/users/:username")
-//       .send({userName : "Sameena", password : "socialgram"})
-//       .end((err,response)=>{
-//         response.should.have.status(400);
-//         done();
-//       });
-//   });
-// });
-
-// describe ("PATCH /", ()=> {
-//   it("Update Password Fail - Wrong User", (done) =>{
-//       chai.request(app)
-//       .patch("/users/:username")
-//       .send({userName : "Sameena", password : "socialgram"})
-//       .end((err,response)=>{
-//         response.should.have.status(403);
-//         response.body.should.have.property('message').eq('You can update only your password');
-//         done();
-//       });
-//   });
-// });
-
-// });
+describe ("PATCH Update Password/", ()=> {
+  it("Update Password Fail - No password", (done) =>{
+      chai.request(app)
+      .patch("/user/" + 'Sameena')
+      .send({userName : "Sameena"})
+      .end((err,response)=>{
+        response.should.have.status(400);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eq('Enter password to update');
+        done();
+      });
+  });
+});
 
 
+describe ("PATCH Update Password/", ()=> {
+  it("Update Password Fail - Bad request", (done) =>{
+      chai.request(app)
+      .patch("/users/" + 'Sameena')
+      .send({userName : "Sameena", password : "socialgram"})
+      .end((err,response)=>{
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
 
-// describe('API Delete User', ()=> {
-//   describe ("DELETE /", ()=> {
-//       it("Delete user success", (done) =>{
-//           chai.request(app)
-//           .delete("/user/:username")
-//           .send({userName : ""})
-//           .end((err,response)=>{
-//             response.should.have.status(200);
-//             response.body.should.be.a('object');
-//             response.body.should.have.property('message').eq('User removed successfully');
-//             done();
-//           });
-//       });
-//   });
+describe ("PATCH Update Password/", ()=> {
+  it("Update Password Fail - Wrong User", (done) =>{
+      chai.request(app)
+      .patch("/user/" + 'Salman')
+      .send({userName : "Sameena", password : "socialgram"})
+      .end((err,response)=>{
+        response.should.have.status(403);
+        response.body.should.have.property('message').eq('You can update only your password');
+        done();
+      });
+  });
+});
 
-//   describe ("DELETE /", ()=> {
-//     it("Delete User Fail - No userName", (done) =>{
-//         chai.request(app)
-//         .delete("/user/:username")
-//         .send({userName : ""})
-//         .end((err,response)=>{
-//           response.body.should.be.a('object');
-//           response.body.should.have.property('message').eq('userName is required');
-//           done();
-//         });
-//     });
-// });
+});
 
 
 
+describe('API Delete User', ()=> {
+  describe ("DELETE User/", ()=> {
+      it("Delete user success", (done) =>{
+          chai.request(app)
+          .delete("/user/" + 'hemaraghu')
+          .send({userName : "hemaraghu"})
+          .end((err,response)=>{
+            response.should.have.status(200);
+            response.body.should.be.a('object');
+            response.body.should.have.property('message').eq('User removed successfully');
+            done();
+          });
+      });
+  });
 
-// describe ("DELETE /", ()=> {
-//   it("Update Password Fail - Bad request", (done) =>{
-//       chai.request(app)
-//       .delete("/users/:username")
-//       .send({userName : ""})
-//       .end((err,response)=>{
-//         response.should.have.status(400);
-//         done();
-//       });
-//   });
-// });
-
-// describe ("PATCH /", ()=> {
-//   it("Delete User Fail - Wrong User", (done) =>{
-//       chai.request(app)
-//       .delete("/users/:username")
-//       .send({userName : ""})
-//       .end((err,response)=>{
-//         response.should.have.status(403);
-//         response.body.should.have.property('message').eq('You can only delete your account');
-//         done();
-//       });
-//   });
-// });
-
-// });
-
-
-
-// describe('API Update Any Information', ()=> {
-//   describe ("PATCH /", ()=> {
-//       it("Update Info Success", (done) =>{
-//           chai.request(app)
-//           .patch('/user/updateany/:username')
-//           .send({userName : "Sameena", password : "tissue"})
-//           .end((err,response)=>{
-//             response.should.have.status(200);
-//             response.body.should.be.a('object');
-//             response.body.should.have.property('message').eq('User Details Updated Successfully');
-//             done();
-//           });
-//       });
-//   });
-
-//   describe ("PATCH /", ()=> {
-//     it("Update Info Fail - No userName", (done) =>{
-//         chai.request(app)
-//         .patch('/user/updateany/:username')
-//         .send({password : "socialgram"})
-//         .end((err,response)=>{
-//           response.body.should.be.a('object');
-//           response.body.should.have.property('message').eq('userName is required');
-//           done();
-//         });
-//     });
-// });
+  describe ("DELETE User/", ()=> {
+    it("Delete User Fail - No userName", (done) =>{
+        chai.request(app)
+        .delete("/user/" + 'deeptha')
+        .send({userName : ""})
+        .end((err,response)=>{
+            response.should.have.status(400);
+            response.body.should.be.a('object');
+            response.body.should.have.property('message').eq('userName is required');
+          done();
+        });
+    });
+});
 
 
 
 
-// describe ("PATCH /", ()=> {
-//   it("Update Any Info Fail - Bad request", (done) =>{
-//       chai.request(app)
-//       .patch('/users/updateany/:username')
-//       .send({userName : "Sameena", password : "socialgram"})
-//       .end((err,response)=>{
-//         response.should.have.status(400);
-//         done();
-//       });
-//   });
-// });
+describe ("DELETE User/", ()=> {
+  it("Delete User Fail - Bad request", (done) =>{
+      chai.request(app)
+      .delete("/users/" + 'deeptha')
+      .send({userName : ""})
+      .end((err,response)=>{
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
 
-// describe ("PATCH /", ()=> {
-//   it("Update Any Info Fail - Wrong User", (done) =>{
-//       chai.request(app)
-//       .patch('/user/updateany/:username')
-//       .send({userName : "Sameena", password : "socialgram"})
-//       .end((err,response)=>{
-//         response.should.have.status(403);
-//         response.body.should.have.property('message').eq('You can update only your account');
-//         done();
-//       });
-//   });
-// });
+describe ("DELETE User/", ()=> {
+  it("Delete User Fail - Wrong User", (done) =>{
+      chai.request(app)
+      .delete("/user/" + 'anusha')
+      .send({userName : 'redmi'})
+      .end((err,response)=>{
+        response.should.have.status(403);
+        response.body.should.have.property('message').eq('You can only delete your account');
+        done();
+      });
+  });
+});
 
-// });
+});
+
+
+
+describe('API Update Any Information', ()=> {
+  describe ("PATCH Update Any Information/", ()=> {
+      it("Update Info Success", (done) =>{
+          chai.request(app)
+          .patch('/user/updateany/' + 'peermohideen')
+          .send({userName : "peermohideen", personalInformation : {firstName : "Peer"}})
+          .end((err,response)=>{
+            response.should.have.status(200);
+            response.body.should.be.a('object');
+            response.body.should.have.property('message').eq('User Details Updated Successfully');
+            done();
+          });
+      });
+  });
+
+  describe ("PATCH Update Any Information/", ()=> {
+    it("Update Info Fail - No userName", (done) =>{
+        chai.request(app)
+        .patch('/user/updateany/' + 'peermohideen')
+        .send({personalInformation : {firstName : "Peer"}})
+        .end((err,response)=>{
+            response.should.have.status(400);
+          response.body.should.be.a('object');
+          response.body.should.have.property('message').eq('userName is required');
+          done();
+        });
+    });
+});
+
+
+
+
+describe ("PATCH Update Any Information/", ()=> {
+  it("Update Any Info Fail - Bad request", (done) =>{
+      chai.request(app)
+      .patch('/users/updateany/' + 'peermohideen')
+      .send({userName : "peermohideen", personalInformation : {firstName : "Peer"}})
+      .end((err,response)=>{
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
+
+describe ("PATCH Update Any Information/", ()=> {
+  it("Update Any Info Fail - Wrong User", (done) =>{
+      chai.request(app)
+      .patch('/user/updateany/' + 'peermohideen')
+      .send({userName : "Aalisha", password : "socialgram"})
+      .end((err,response)=>{
+        response.should.have.status(403);
+        response.body.should.have.property('message').eq('You can update only your account');
+        done();
+      });
+  });
+});
+
+});
