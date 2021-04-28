8a1f0068647a20fcf5f4c53a8500fee1305953bf
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-04-28 19:53:11 +0530

    Update Postdetails.js

--- Model/Postdetails.js
+++ Model/Postdetails.js
@@ -1,31 +1,20 @@
 const mongoose = require('mongoose');
-//var validate = require('mongoose-validator');
-
 const postschema = mongoose.Schema({ 
     userName :{
         type : String,
         required : true
     },
     status : {
-        type : Array ,
-        items : {
-            type : String ,
-            max: 20 ,
-            required : true
-        },
-    },
-    likes : {
-        type : Array ,
-        default : []
+        type : String ,
+        max: 150 ,
+        required : true
     }
-},
+},  
 { timestamps :{ createdAt: 'created_at',updatedAt: 'updated_at' }}
 );
 
-// var stringValidator = [ validate({validator : 'isLength' , arguments : [1,150] , message : 'Max size is 150 characters'})]
 postschema.path('status').validate(function(v){
-    console.log(v);
-    return v.length <=20;
+    return v.length <=150;
 },"The maximum length is 150 ");
 
 module.exports = mongoose.model('Post' , postschema);
