de745723fc045e6615480934d36b294d97311ac9
parveen99 <40237237+parveen99@users.noreply.github.com>
2021-05-05 13:32:18 +0530

    Update userAPI.js

--- routes/userAPI.js
+++ routes/userAPI.js
@@ -1,178 +1,98 @@
-const e = require('express');
-let express = require('express');
-let router = express.Router();
-let userDetails = require('../Model/userDetails');
+const mongoose = require('mongoose');
+const arrayValidator = require ('mongoose-array-validator');
 
-//USER SIGNUP
-router.post('/signup' ,async (req,res) => {
-    try{
-        let emailExist = await userDetails.findOne({email : req.body.email});
-        if(emailExist){
-            res.status(409).json({message : "Email already exists ! Signup with a different EmailID"});
-        }
-        else if(req.body.email && req.body.personalInformation.DOB){
-            let email = req.body.email;
-            let DOB = req.body.personalInformation.DOB ;
-            let signupNew = new userDetails({
-                email : req.body.email ,
-                userName : email.substring(0,email.lastIndexOf("@")),
-                personalInformation : {
-                    firstName : req.body.personalInformation.firstName ,
-                    lastName : req.body.personalInformation.lastName ,
-                    phoneNumber : req.body.personalInformation.phoneNumber,
-                    DOB : req.body.personalInformation.DOB,
-                    age : calculateAge(new Date(DOB))
-                } ,
-                address : {
-                    street : req.body.address.street ,
-                    city : req.body.address.city ,
-                    pincode : req.body.address.pincode,
-                    state : req.body.address.state,
-                    country : req.body.address.country
-                }
-            });
-            let userName = signupNew.userName
-            let password = signupNew.password
-            try {
-                await signupNew.save() 
-                res.status(201).json({USERNAME : userName , PASSWORD : password });
-                } catch (err) {
-                    res.status(500).json({message : err});
-                }
-        }
-        else {
-            res.status(400).json({message : "Email ID / DOB not given in data"});
-        }
-    }catch(err) {
-        res.status(400).json({message:err});
-    }  
-});
+const userschema = mongoose.Schema({
 
-//LOGIN USING USERNAME / PASSWORD
-router.get('/login' ,async (req,res) => {
-    if(req.body.email){
-        var checkUserFromDBUsingEmail = await userDetails.findOne({email : req.body.email});
-        if(checkUserFromDBUsingEmail){
-            if(req.body.email === checkUserFromDBUsingEmail.email){
-                if(req.body.password === checkUserFromDBUsingEmail.password){
-                    res.status(200).json({message : "Login Successfull"});
-                }
-                else {
-                    res.status(401).json({message : "Password Incorrect"});
-                }
-            }
-        }
-        else{
-            res.status(400).json({message : "User not found! Invalid emailID"});
-        }
-    }
-    else if(req.body.userName){
-        var checkUserFromDBUsingUsername = await userDetails.findOne({userName : req.body.userName});
-        if(checkUserFromDBUsingUsername){
-            if(req.body.userName === checkUserFromDBUsingUsername.userName){
-                if(req.body.password === checkUserFromDBUsingUsername.password){
-                    res.status(200).json({message : "Login Successfull"});
-                }
-                else {
-                    res.status(401).json({message : "Password Incorrect"});
-                }
-            }
-        }
-        else{
-            res.status(400).json({message : "User not found! Invalid Username"});
-        }
-    }
-});
+    email : {
+        type : String,
+        trim : true ,
+        required: true ,
+        unique : true
+    },
 
-//UPDATING PASSWORD
-router.patch('/updatePassword' ,async (req,res) => {
-    if(!req.body.userName){
-        res.status(400).json({message : "userName is required"});
-    }
-    else if(!req.body.oldPassword){
-        res.status(400).json({message : "OldPassword is required to update"});
-    }
-    else if(!req.body.newPassword){
-        res.status(400).json({message : "Please enter NewPassword"});
-    }
-    else if(req.body.oldPassword === req.body.newPassword){
-        res.status(400).json({message : "OldPassword and NewPassword are same"});
-    }
-    else{
-        const validUser = await userDetails.findOne({userName : req.body.userName ,password : req.body.oldPassword});
-        if(validUser){
-            try{
-                await userDetails.updateOne(
-                    {userName : req.body.userName} ,
-                    {$set:{password : req.body.newPassword}}
-                );
-                res.status(200).json({message : "Password updated successfully"});
-            }
-            catch (err){
-                res.status(500).json({message : err});
-            }
+    userName : {
+        type : String,
+        unique : true
+    },
+    
+    password :{
+        type : String ,
+        trim : true ,
+        default : 'social'
+    },
+
+    personalInformation : {
+        firstName : {
+            type : String ,
+            required : true ,
+            trim : true 
+        },
+    
+        lastName : {
+            type : String ,
+            trim : true
+        },
+
+        phoneNumber : {
+            type : Array ,
+            minItems : 1 ,
+            maxItems : 2 ,
+            items : {
+                type : Number
+            },
+            uniqueItems:true
+        },
+
+        DOB : {
+            type : String ,
+            trim : true ,
+            required : true
+        },
+
+        age : {
+            type : Number
         }
-        else{
-            res.status(400).json({message : "User not found! Invalid Credentials"});
-        }
-    }
-});
+    },
+    
+    address : {
+        street : {
+            type : String,
+            trim : true 
+        },
 
+        city : {
+            type : String ,
+            trim : true 
+        },
 
+        pincode :{
+            type : Number ,
+            trim : true ,
+            required : true
+        },
 
+        state : {
+            type : String ,
+            trim : true ,
+            required : true
+        },
 
-//UPDATING ANY INFORMATION
-router.patch('/updateUserInformation' ,async (req,res) => {
-    if(!req.body.userName){
-        res.status(400).json({message : "userName is required"})
-    }
-    else{
-        const validUser = await userDetails.findOne({userName : req.body.userName ,password : req.body.password});
-        if(validUser){
-            try{
-                await userDetails.findOneAndUpdate(
-                    {userName:req.body.userName},
-                    {$set:req.body});
-                res.status(200).json({message : "User Details Updated Successfully"});
-            }
-            catch (err) {
-                res.status(500).json({message : err});
-            }
-        }
-        else{
-            res.status(400).json({message : "User not found! Invalid Credentials"});
-        }
-    }
-});
-
-
-//DELETING A USER
-router.delete('/deleteUser' ,async (req,res) => {
-    if(!req.body.userName){
-        res.status(400).json({message : "userName is required"});
-    }
-    else{
-        const validUser = await userDetails.findOne({userName : req.body.userName ,password : req.body.password});
-        if(validUser){
-            try{
-                await userDetails.deleteOne({userName : req.body.userName });
-                res.status(200).json({message : "User removed successfully"});
-            }
-            catch (err){
-                res.status(500).json({message : err});
-            }
-        }
-        else{
-            return res.status(400).json({message : "User not found! Invalid Credentials"});
+        country : {
+            type : String ,
+            trim : true ,
+            required : true
         }
     }
-});
-
-//Calcuate Age from DOB
-function calculateAge(DOB){
-    var ageDifMs = Date.now() - DOB.getTime();
-    var ageDate = new Date(ageDifMs);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+},
+{
+    timestamps : true
 }
+// {
+//     strict : false ,
+//     collection : 'user'
+// }
 
-module.exports = router;
+);
+userschema.plugin(arrayValidator);
+    
+module.exports = mongoose.model('user' , userschema);
